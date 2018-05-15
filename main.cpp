8dab6dec55d1ab473cfac875b8a37df2b951b011
u211-01 <u211-01@public.mpei.local>
2018-05-15 11:31:32 +0300

    noviy commit

--- main.cpp
+++ main.cpp
@@ -57,6 +57,8 @@ print_in_binary(const void* data, size_t size) {
 }
 int main()
 {
+    uint16_t oper1, oper3, res;
+    char oper2;
     assert(nibble_to_hex(0x0) == '0');
     assert(nibble_to_hex(0x1) == '1');
     assert(nibble_to_hex(0x2) == '2');
@@ -73,11 +75,56 @@ int main()
     assert(nibble_to_hex(0xd) == 'd');
     assert(nibble_to_hex(0xe) == 'e');
     assert(nibble_to_hex(0xf) == 'f');
-    uint16_t u32 = 0x42;
-    cout << "u32 bytes: ";
-    print_in_hex(&u32, sizeof(u32));
-    cout << '\n';
-    uint8_t u8 =3;
-    print_in_binary(&u8, sizeof(u8));
-    cout << '\n';
+    cout<<"Vvedite 16rich";
+    cin>>oper1>>oper2>>oper3;
+    switch(oper2)
+    {
+    case '&':
+        res=oper1&oper3;
+        print_in_hex(&oper1, sizeof(oper1));
+        cout<<oper2;
+        print_in_hex(&oper3, sizeof(oper3));
+        cout<<" = ";
+        print_in_hex(&res, sizeof(res));
+        cout<<'\n';
+        print_in_binary(&oper1, sizeof(oper1));
+        cout<<oper2<<" ";
+        print_in_binary(&oper2, sizeof(oper2));
+        cout<<" = ";
+        print_in_binary(&res, sizeof(res));
+        cout<<'\n';
+break;
+
+case '|':
+
+res=oper1|oper3;
+print_in_hex(&oper1, sizeof(oper1));
+cout<<oper2;
+print_in_hex(&oper3, sizeof(oper3));
+cout<<" = ";
+print_in_hex(&res, sizeof(res));
+cout<<'\n';
+print_in_binary(&oper1, sizeof(oper1));
+cout<<oper2<<" ";
+print_in_binary(&oper2, sizeof(oper2));
+cout<<" = ";
+print_in_binary(&res, sizeof(res));
+cout<<'\n';
+
+    case'^':
+res=oper1^oper3;
+print_in_hex(&oper1, sizeof(oper1));
+cout<<oper2;
+print_in_hex(&oper3, sizeof(oper3));
+cout<<" = ";
+print_in_hex(&res, sizeof(res));
+cout<<'\n';
+print_in_binary(&oper1, sizeof(oper1));
+cout<<oper2<<" ";
+print_in_binary(&oper2, sizeof(oper2));
+cout<<" = ";
+print_in_binary(&res, sizeof(res));
+cout<<'\n';
+
+}
 }
