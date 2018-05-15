cf00a10c75eba5c4b4036f90ca94d8df0141b56f
u211-01 <u211-01@public.mpei.local>
2018-05-15 10:50:43 +0300

    new commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <cmath>
 #include <cassert>
 using namespace std;
-char nible_to_hex(uint8_t i)
-{
-    char digits[]="0123456789abcdef";
+char nibble_to_hex(uint8_t i) {
+    assert(0x0 <= i && i <= 0xf);
+    char digits[] = "0123456789abcdef";
     return digits[i];
 }
-void print_to_hex(uint8_t byte)
+void print_in_hex(uint8_t byte)
 {
-    cout << nible_to_hex(byte>>4);
-    cout << nible_to_hex(byte&0b00001111);
+    cout << nibble_to_hex(byte >> 4);
+    cout<< nibble_to_hex(byte & 0b00001111);
 }
 const uint8_t*
 as_bytes(const void* data) {
@@ -21,8 +20,34 @@ void print_in_hex(const void* data, size_t size) {
     for (size_t i = 0; i < size; i++) {
         print_in_hex(bytes[i]);
 
-        // Для удобства чтения: пробелы между байтам, по 16 байт на строку.
         if ((i + 1) % 16 == 0) {
+            cout << '\n';
+        }
+        else {
+            cout << ' ';
+        }
+    }
+}
+char bit_digit(uint8_t byte, uint8_t bit) {
+    if (byte & (0x1 << bit)) {
+        return '1';
+    }
+    return '0';
+}
+void print_in_binary(uint8_t byte) {
+    for (uint8_t bit = 7; bit > 0; bit--) {
+        cout << bit_digit(byte, bit);
+    }
+    cout<<bit_digit(byte,0);
+}
+
+void
+print_in_binary(const void* data, size_t size) {
+    const uint8_t* bytes = as_bytes(data);
+    for (size_t i = 0; i < size; i++) {
+        print_in_binary(bytes[i]);
+
+        if ((i + 1) % 4 == 0) {
             cout << '\n';
         }
         else {
@@ -32,21 +57,27 @@ void print_in_hex(const void* data, size_t size) {
 }
 int main()
 {
-    assert(nible_to_hex(0x0) == '0');
-    assert(nible_to_hex(0x1) == '1');
-    assert(nible_to_hex(0x2) == '2");
-    assert(nible_to_hex(0x3) == '3');
-    assert(nible_to_hex(0x4) == '4');
-    assert(nible_to_hex(0x5) == '5');
-    assert(nible_to_hex(0x6) == '6');
-    assert(nible_to_hex(0x7) == '7');
-    assert(nible_to_hex(0x8) == '8');
-    assert(nible_to_hex(0x9) == '9');
-    assert(nible_to_hex(0xa) == 'a');
-    assert(nible_to_hex(0xb) == 'b');
-    assert(nible_to_hex(0xc) == 'c');
-    assert(nible_to_hex(0xd) == 'd');
-    assert(nible_to_hex(0xe) == 'e');
-    assert(nible_to_hex(0xf) == 'f');
-
+    assert(nibble_to_hex(0x0) == '0');
+    assert(nibble_to_hex(0x1) == '1');
+    assert(nibble_to_hex(0x2) == '2');
+    assert(nibble_to_hex(0x3) == '3');
+    assert(nibble_to_hex(0x4) == '4');
+    assert(nibble_to_hex(0x5) == '5');
+    assert(nibble_to_hex(0x6) == '6');
+    assert(nibble_to_hex(0x7) == '7');
+    assert(nibble_to_hex(0x8) == '8');
+    assert(nibble_to_hex(0x9) == '9');
+    assert(nibble_to_hex(0xa) == 'a');
+    assert(nibble_to_hex(0xb) == 'b');
+    assert(nibble_to_hex(0xc) == 'c');
+    assert(nibble_to_hex(0xd) == 'd');
+    assert(nibble_to_hex(0xe) == 'e');
+    assert(nibble_to_hex(0xf) == 'f');
+    uint16_t u32 = 0x42;
+    cout << "u32 bytes: ";
+    print_in_hex(&u32, sizeof(u32));
+    cout << '\n';
+    uint8_t u8 =3;
+    print_in_binary(&u8, sizeof(u8));
+    cout << '\n';
 }
